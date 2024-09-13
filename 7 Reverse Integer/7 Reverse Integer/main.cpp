628a12562e450701792c8ecfd7e1ab3673dd327c
youshum1tsy <balaev.gleb@mail.ru>
2024-09-13 18:18:35 +0300

    7
    https://leetcode.com/problems/reverse-integer/

---
+++ 7 Reverse Integer/7 Reverse Integer/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+
+            if (result > INT_MAX / 10 ||
+                (result == INT_MAX / 10 && pop > 7)) {
+                return 0;
+            }
+            if (result < INT_MIN / 10 ||
+                (result == INT_MIN / 10 && pop < -8)) {
+                return 0;
+            }
+            result = result * 10 + pop;
+        }
+        return result;
+    }
+};
+int main() {
+    int x = 1534236469;
+    Solution solve;
+    std::cout << solve.reverse(x);
+}
