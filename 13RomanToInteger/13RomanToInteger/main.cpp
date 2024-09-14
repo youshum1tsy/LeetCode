c5615f4854e67df274c4f2e87624f050a95d6949
youshum1tsy <balaev.gleb@mail.ru>
2024-09-14 13:48:37 +0300

    13
    https://leetcode.com/problems/roman-to-integer/description/

--- 13RomanToInteger/13RomanToInteger/main.cpp
+++ 13RomanToInteger/13RomanToInteger/main.cpp
@@ -3,7 +3,6 @@
 
 class Solution {
 public:
-
     int romanToInt(std::string s) {
         std::map<char, int> romanNumbers = {
             {'I', 1},
@@ -15,33 +14,51 @@ public:
             {'M', 1000},
         };
         int result = 0;
-        for (int i = 0; i < s.size(); i++ ) {
+        for (int i = 0; i < s.size(); i++) {
             if (s[i] == 'I') {
-                if (s[i+1] == 'V') {
+                if (s[i + 1] == 'V') {
                     result = result + 4;
+                    i++;
                 }
-                else if (s[i+1] == 'X') {
+                else if (s[i + 1] == 'X') {
                     result = result + 9;
+                    i++;
+                }
+                else {
+                    result = result + 1;
                 }
             }
             else if (s[i] == 'X') {
                 if (s[i + 1] == 'L') {
                     result = result + 40;
+                    i++;
                 }
                 else if (s[i + 1] == 'C') {
                     result = result + 90;
+                    i++;
+                }
+                else {
+                    result = result + 10;
                 }
             }
             else if (s[i] == 'C') {
                 if (s[i + 1] == 'D') {
                     result = result + 400;
+                    i++;
                 }
                 else if (s[i + 1] == 'M') {
                     result = result + 900;
+                    i++;
+                }
+                else {
+                    result = result + 100;
                 }
             }
-            result = result + romanNumbers[s[i]];
+            else {
+                result = result + romanNumbers[s[i]];
+            }
         }
+        return result;
     }
 };
 
@@ -49,10 +66,11 @@ int main() {
     std::string s1 = "III";
     std::string s2 = "LVIII";
     std::string s3 = "MCMXCIV";
+    std::string s4 = "DCXXI";
     Solution solve;
     std::cout << solve.romanToInt(s1) << std::endl;
     std::cout << solve.romanToInt(s2) << std::endl;
     std::cout << solve.romanToInt(s3) << std::endl;
-
+    std::cout << solve.romanToInt(s4) << std::endl;
 
 }
