8e19109d13a521b50f3ced75d5c4dc9f2e73cc86
youshum1tsy <balaev.gleb@mail.ru>
2024-09-23 16:12:41 +0300

    69
    https://leetcode.com/problems/sqrtx/description/

---
+++ 69Sqrt(x)/69Sqrt(x)/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int start = 0;
+        int end = 46341;
+        if (x == 1) {
+            return 1;
+        }
+        while (end - start != 1) {
+            int mid = (start + end) / 2;
+            if (mid * mid > x) {
+                end = mid;
+            }
+            else if (mid * mid < x) {
+                start = mid;
+            }
+            else if (mid * mid == x) {
+                return mid;
+            }
+        }
+        return start;
+
+    }
+};
+
+int main() {
+    int number = 1;
+    Solution solve;
+    std::cout << solve.mySqrt(number);
+}
